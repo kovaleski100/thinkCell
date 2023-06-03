f136455fdd3d7e34edb2cb2a5137f4bd64e7adb8
kovaleski100 <mfkovaleski@inf.ufrgs.br>
2023-06-03 19:45:11 -0300

    update

--- interval_map.cpp
+++ interval_map.cpp
@@ -23,56 +23,54 @@ public:
             return ;
         }
 
-        auto it = m_map.upper_bound(keyBegin);
+    auto it = m_map.upper_bound(keyBegin);
 
-        if(it == m_map.begin() && (keyBegin < it->first || it == m_map.end()))
-        {   
-            if(m_valBegin == val)
-            {
-                return;
-            }
-            m_map.insert(it, {keyBegin, val});
-        }
-        else if(std::prev(it)->second == val)
+    if(it == m_map.begin() && (keyBegin < it->first || it == m_map.end())) 
+    {
+        if(m_valBegin == val)
         {
             return;
         }
-        else if(it != m_map.end() && keyEnd < it->first && it->second == val)
-        {
-            return;
+        m_map.insert(it, { keyBegin, val });
+    }
+
+    // Check if keyBegin falls within the previous interval
+    else if (std::prev(it)->second == val)
+        return;
+
+    // Check if keyEnd falls within the current interval
+    else if (it != m_map.end() && keyEnd < it->first && it->second == val)
+        return;
+
+    // Insert or update the intervals as necessary
+    else {
+        V prevVal = std::prev(it)->second;
+        if (prevVal == val) {
+            // Merge with the previous interval if it has the same value
+            --it;
+            K newKeyEnd = std::max(keyEnd, it->first);
+            it = m_map.erase(it);
+            if (newKeyEnd > keyBegin)
+                m_map.insert(it, { newKeyEnd, prevVal });
         }
-        else
-        {
-            V const& prevVal = std::prev(it)->second;
-            if(prevVal == val)
-            {
-                --it;
-                K newKeyEnd = std::max(keyEnd, it->first);
-                it = m_map.erase(it);
-                if(newKeyEnd > keyBegin)
-                {
-                    m_map.insert(it, {newKeyEnd, prevVal});
-                }
-            }
-            while(it != m_map.end() && it->second == val && keyEnd > it->first)
-            {
-                it = m_map.erase(it);
-            }
-            if(it != m_map.end() && keyEnd > it->first && it->second == val)
-            {
-                K newKeyEnd = std::max(keyEnd, it->first);
-                it = m_map.erase(it);
-                if(newKeyEnd > keyBegin)
-                {
-                    m_map.insert(it, {newKeyEnd, prevVal});
-                }
-            }
-            m_map.insert(it, {keyBegin, val});
-            if(keyEnd > keyBegin)
-            {
-                m_map.insert(it, {keyEnd, prevVal});
-            }
+
+        while (it != m_map.end() && it->second == val && keyEnd > it->first) {
+            // Erase intervals completely covered by the new interval
+            it = m_map.erase(it);
         }
+
+        if (it != m_map.end() && keyEnd > it->first && it->second == val) {
+            // Merge with the following interval if it has the same value
+            K newKeyEnd = std::max(keyEnd, it->first);
+            it = m_map.erase(it);
+            if (keyBegin < newKeyEnd)
+                m_map.insert(it, { newKeyEnd, prevVal });
+        }
+
+        m_map.insert(it, { keyBegin, val }); // Insert the new interval
+        if (keyBegin < keyEnd > keyBegin)
+            m_map.insert(it, { keyEnd, prevVal }); // Insert the end of the previous interval
+    }
 
     }
 
@@ -90,8 +88,8 @@ public:
 int main() {
     interval_map<int, char> M('A');
 
-    //M.assign(1, 3, 'B');
-    M.assign(5, 7, 'C');
+    M.assign(1, 3, 'B');
+    //M.assign(5, 7, 'C');
     //M.assign(2, 6, 'D');
 
     for (int i = -2; i <= 8; i++) {
